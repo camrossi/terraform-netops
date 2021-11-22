0063037856b04fb875c50729d2afc22bb011f064
Camillo <camrossi@cisco.com>
2021-11-22 15:27:14 +1100

    commit

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "tenant_name" {
-  value = aci_tenant.terraform_ten.name
+  value = aci_tenant.tenant.name
 }
 
 output "vrf_name" {
-  value = aci_vrf.vrf1.name
+  value = aci_vrf.vrf.name
 }
 
 output "bd_name" {   
-  value = aci_bridge_domain.bd1.name            
+  value = aci_bridge_domain.bd.name            
 }
